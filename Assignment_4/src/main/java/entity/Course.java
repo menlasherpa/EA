b17c7d5fef3dbbdfd16bb560ae802e10fdf99a72
Lakpa Sherpa <lakpadoma97@gmail.com>
2023-04-02 23:53:47 -0500

    Add files via upload

---
+++ Assignment_4/src/main/java/entity/Course.java
@@ -0,0 +1,69 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "COURSE_TYPE")
+@Cacheable(value = true)
+public abstract class Course {
+
+    @Id
+    @GeneratedValue
+    private long courseId;
+
+    private String title;
+
+    private LocalDate startDate;
+
+    private String professorName;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    private Student student;
+
+    public Course() {
+        super();
+    }
+
+    public Course(long courseId, String title, LocalDate startDate, String professorName) {
+        super();
+        this.courseId = courseId;
+        this.title = title;
+        this.startDate = startDate;
+        this.professorName = professorName;
+    }
+
+    public long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
+}
